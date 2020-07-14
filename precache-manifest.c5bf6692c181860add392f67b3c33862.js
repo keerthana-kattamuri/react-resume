795dfbfe4921be831b81a57b2645a575af29f978
keerthana <keerthana.kattamuri@gmail.com>
2020-07-14 21:37:38 +0530

    Updates

---
+++ precache-manifest.c5bf6692c181860add392f67b3c33862.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ebccc51354bc6ce9a39",
+    "url": "/react-resume/static/js/runtime~main.bbd03709.js"
+  },
+  {
+    "revision": "e926eb1cc971b23cb355",
+    "url": "/react-resume/static/js/main.8865a7cb.chunk.js"
+  },
+  {
+    "revision": "f6d2ab47bb337dc705a8",
+    "url": "/react-resume/static/js/2.b67fbdca.chunk.js"
+  },
+  {
+    "revision": "e926eb1cc971b23cb355",
+    "url": "/react-resume/static/css/main.ce8cec51.chunk.css"
+  },
+  {
+    "revision": "0784a0c899b5506d6440b804b9d39ef6",
+    "url": "/react-resume/index.html"
+  }
+];
